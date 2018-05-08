336c683d39574399e192ca58b6575843e1d73dd4
Cristiano Pacheco <chris.spb25@gmail.com>
2018-05-07 22:13:16 -0300

    Added text for home component

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Header, Icon, Message } from 'semantic-ui-react'
 
 const Home = () => (
-  <div>Home</div>
+  <Fragment>
+    <Header as='h2' icon textAlign='center'>
+      <Icon name='trophy' circular />
+      <Header.Content>
+        Client Side Challenge
+      </Header.Content>
+    </Header>
+    <Message size='huge' style={{ textAlign: 'justify' }}>
+      This client consume the API developed in the final challenge of the training Immersion in Development of APIS with
+      the monster Erick Wendel.
+    </Message>
+  </Fragment>
 )
 
 export default Home
